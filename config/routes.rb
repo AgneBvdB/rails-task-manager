852ba5c470c7207443bc4c213059e3651ed0b826
Agne Banzeryte <Agne.banzeryte@gmail.com>
2026-02-21 13:27:57 +0100

    manual crud finished

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # CREATE
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # READ
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+  # UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
